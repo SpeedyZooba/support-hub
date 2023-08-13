a9646a9e6ca739fa51c73aefa09a99fb8477968b
Gorkem Revi <gorkemrevi@hotmail.com>
2023-08-13 14:43:56 +0300

    minor adjustments to redirection and frontend

--- src/main/java/com/projects/supporthub/system/LoginController.java
+++ src/main/java/com/projects/supporthub/system/LoginController.java
@@ -2,6 +2,9 @@ package com.projects.supporthub.system;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,6 +16,11 @@ public class LoginController
     @GetMapping("/login")
     public String login()
     {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getPrincipal() instanceof UserDetails)
+        {
+            return "redirect:/home";
+        }
         log.info("Login page initiated.");
         return "login";
     }
